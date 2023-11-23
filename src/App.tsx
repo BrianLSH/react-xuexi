6151c736d9e713f5aa97f31c8e3f5ac5f9189d24
lsh <923717838@qq.com>
2023-11-23 12:57:21 +0800

    4 svg 引入

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // 组件首字母小写报错
 import ShowName from "./component/test1";
 import './App.css';
+import logo from './asset/images/logo.svg'
 import nameData from './mockData/test.json';
 // import styles from './test2.module.css';
 // import styles from "./test1";
@@ -11,8 +12,9 @@ import styles from './test1.module.css'
 function App() {
   return (
       <div className={styles.sort}>
+          <img src="logo" className={styles.logoStyle} alt="logo"/>
           <div className={styles.letterColor}>
-              {nameData.map(item => <ShowName id={item.id} name={item.name} skills={item.skill}></ShowName>)}
+              {nameData.map(item => <ShowName key={item.id} id={item.id} name={item.name} skills={item.skill}></ShowName>)}
           </div>
       </div>
 
