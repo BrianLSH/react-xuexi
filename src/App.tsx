df6bba07592849f8be29aa56b1c7b1f44b64d084
lsh <923717838@qq.com>
2023-11-23 12:23:06 +0800

    3. css in js   jss 的使用

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import ShowName from "./component/test1";
 import './App.css';
 import nameData from './mockData/test.json';
+// import styles from './test2.module.css';
+// import styles from "./test1";
+// import test2 from "./test2";
+import styles from './test1.module.css'
+
 function App() {
   return (
-   <div>
-     {nameData.map(item => <ShowName id={item.id} name={item.name} skills={item.skill}></ShowName>)}
-   </div>
+      <div className={styles.sort}>
+          <div className={styles.letterColor}>
+              {nameData.map(item => <ShowName id={item.id} name={item.name} skills={item.skill}></ShowName>)}
+          </div>
+      </div>
+
   );
 }
 
